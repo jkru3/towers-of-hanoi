0b520b50e2d7f25d46583795c47209bc1d331562
jkru3 <josephkrueger@protonmail.com>
2022-05-23 22:25:48 -0700

    first commit

---
+++ src/JKtowersOfHanoi.java
@@ -0,0 +1,15 @@
+public class JKtowersOfHanoi
+{
+    public static void main(String[] args)
+    {
+        int rings = 3;
+        int moves = tower(rings);
+        System.out.println("With " + rings + " rings, the tower is solved in " +
+                           moves + " moves");
+    }
+    
+    public static int tower(int rings)
+    {
+        return 5;
+    }
+}
