ba59abfd7570b25813df276f09d61e555488b414
jkru3 <josephkrueger@protonmail.com>
2022-05-24 19:01:19 -0700

    first working iteration with enum

--- src/JKtowersOfHanoi.java
+++ src/JKtowersOfHanoi.java
@@ -4,24 +4,89 @@
 //
 //
 
-import java.util.ArrayList;
 import java.util.*;
-import java.util.Stack;
 
 public class JKtowersOfHanoi
 {
     public enum rod
     {
-        A,
-        B,
-        C;
+        A(a), B(b), C(c);
+    
+        private Stack<Boolean> anyStack;
+        
+        rod(Stack<Boolean> anyStack)
+        {
+            this.anyStack = anyStack;
+        }
+        
+        public void enumPush(boolean pop)
+        {
+            anyStack.push(pop);
+        }
+    
+        private Boolean enumPop()
+        {
+            return anyStack.pop();
+        }
+    
     }
+    
+    public static Stack<Boolean> a = new Stack<>();
+    public static Stack<Boolean> b = new Stack<>();
+    public static Stack<Boolean> c = new Stack<>();
+    public static Stack aRef = a;
+    public static Stack bRef = b;
+    public static Stack cRef = c;
     
     public static void main(String[] args)
     {
+        int intRing = menu();
+        for(int i = intRing; i >= 1; i--)
+            a.push(true);
+        rod start = rod.A;
+        rod transfer = rod.B;
+        rod destination = rod.C;
+        tower(intRing, start, transfer, destination, a, b, c);
+    }
+    
+    static void tower(int intRing,
+                      rod start,
+                      rod transfer,
+                      rod destination,
+                      Stack a,
+                      Stack b,
+                      Stack c)
+    {
+        if (intRing == 1)
+        {
+            destination.enumPush(start.enumPop());
+            System.out.println("Move ring 1: " +  start + " -> " + destination +
+                               "\t\t[" + aRef.size() + "] [" + bRef.size() + "] [" + cRef.size() + "]");
+        } else {
+            tower(intRing - 1,
+                  start,
+                  destination,
+                  transfer,
+                  a, c, b);
+    
+            destination.enumPush(start.enumPop());
+            System.out.println("Move ring " + intRing + ": " +
+                                start + " -> " + destination +
+                               "\t\t[" + aRef.size() + "] [" + bRef.size() + "] [" + cRef.size() + "]");
+            
+            tower(intRing - 1,
+                  transfer,
+                  start,
+                  destination,
+                  b, a, c);
+        }
+    }
+    
+    public static int menu()
+    {
         int intRing = 0;
         Scanner input = new Scanner(System.in);
-        System.out.print("\nHow many rings would you like to test? (Max 15): ");
+        System.out.print("How many rings would you like to test? (Max 15): ");
         while(intRing < 1 || intRing > 15)
         {
             String stringRing = input.nextLine();
@@ -34,30 +99,6 @@ public class JKtowersOfHanoi
                 System.out.println("Please enter a number between 1 and 15: ");
             }
         }
-        
-        char l__ = 'A'; // this rod is always the start
-        char _l_ = 'B'; // this rod is always the middle auxilery rod
-        char __l = 'C'; // this rod is always the end
-        tower(intRing, l__, _l_, __l);
-        System.out.println("done!");
-    }
-    
-    static void tower(int intRing, char l__, char _l_, char __l)
-    {
-        if (intRing == 1)
-        {
-            //this prints every other since ring 1 must be moved every other turn
-            System.out.println("Move ring 1: " +  l__ + " -> " + __l);
-        } else {
-            //notice how this recursion mixes up the B and C rods
-            //on even ring counts, the top ring is sent to the middle auxilery rod first
-            //on odd ring counts, the top ring is sent to the end rod
-            tower(intRing - 1, l__, __l, _l_);
-            
-            //
-            System.out.println("Move ring " + intRing +
-                               ": " + l__ + " -> " + __l);
-            tower(intRing - 1, _l_, l__, __l);
-        }
+        return intRing;
     }
 }
